a1091b2c1873d435b08f46f4b8aa2f8fa62912a8
Анна Г. <125292031+MissSnape@users.noreply.github.com>
2025-11-27 15:40:35 +0400

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './test', // ИЛИ '/your-repo-name/' если знаете имя репозитория
+  base: '/prjct/', // Замените 'prjct' на имя вашего репозитория
+  build: {
+    outDir: 'docs'
+  }
 })
 
